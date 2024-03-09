fc51d9531cab12fc0bbefee4e4ef5916611a92fc
Midori Takahashi <mintgreen7programming@gmail.com>
2024-03-09 15:23:22 +0900

    Add D1 and users table

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import build from '@hono/vite-cloudflare-pages'
 import devServer from '@hono/vite-dev-server'
 import { defineConfig } from 'vite'
+import { getPlatformProxy } from 'wrangler'
 
-export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      entry: 'src/index.tsx'
-    })
-  ]
+export default defineConfig(async ({ command }) => {
+  if (command === 'build') {
+    return {
+      plugins: [build()],
+    }
+  }
+  const { env, dispose } = await getPlatformProxy()
+  return {
+    plugins: [
+      devServer({
+        entry: 'src/index.tsx',
+        adapter: {
+          env,
+          onServerClose: dispose,
+        },
+      }),
+    ],
+  }
 })
