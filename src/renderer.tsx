e411be7e7fb4c70ee4932ea8a7db23d946dcce3e
Midori Takahashi <mintgreen7programming@gmail.com>
2024-03-16 05:06:38 +0900

    path

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -4,7 +4,7 @@ export const renderer = jsxRenderer(({ children, title }) => {
   return (
     <html>
       <head>
-        <link href="dist/static/style.css" rel="stylesheet" />
+        <link href="/static/style.css" rel="stylesheet" />
           <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com"/>
         <title>{title}</title>
       </head>
