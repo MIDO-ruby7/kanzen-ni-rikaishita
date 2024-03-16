17976fd178331f4409d8e9f9dbc6982ac7d3d8b1
Midori Takahashi <mintgreen7programming@gmail.com>
2024-03-16 19:45:34 +0900

    Add HTMX

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -4,8 +4,8 @@ export const renderer = jsxRenderer(({ children, title }) => {
   return (
     <html>
       <head>
-        <link href="/static/style.css" rel="stylesheet" />
-          <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com"/>
+        <link href="dist/static/style.css" rel="stylesheet" />
+        <script src="https://unpkg.com/htmx.org@1.9.11" integrity="sha384-0gxUXCCR8yv9FM2b+U3FDbsKthCI66oH5IA9fHppQq9DDMHuMauqq1ZHBpJxQ0J0" crossorigin="anonymous"></script>
         <title>{title}</title>
       </head>
       <body>{children}</body>
--- src/views/static/top.tsx
+++ src/views/static/top.tsx
@@ -20,19 +20,14 @@ export const Top = () => {
               完全に <br />
               <span class="text-teal-400">理解した</span>
             </h1>
-            <h2 class="w-full text-xl font-bold mt-5">
+            <h2 class="text-xl font-bold mt-5">
               <span class="text-stone-800">理解のスタート地点から、進化を始めよう。</span>
             </h2>
-            <script src="https://accounts.google.com/gsi/client" async defer></script>
-            <button type="button" class="w-1/3 md:w-1/3 sm:w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300 mt-20">
-            <div class="flex items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="w-6 h-6" viewBox="0 0 48 48"><defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/></defs><clipPath id="b"><use xlink:href="#a" overflow="visible"/></clipPath><path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z"/><path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z"/><path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z"/><path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z"/></svg>
-            <span class="ml-4">
-            Log in
-            with
-            Google</span>
-            </div>
-          </button>
+            <a href="/posts">
+              <button class="before:ease relative h-12 w-40 mt-10 ml-5 overflow-hidden border border-teal-500 bg-teal-500 text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-teal-500 hover:before:-translate-x-40">
+                <span relative="relative z-10">START</span>
+              </button>
+            </a>
           </div>
         </div>
       </section>
@@ -43,7 +38,8 @@ export const Top = () => {
               を登る
             </h1>
             <h2 class="w-full text-xl font-bold mt-5">
-              <span class="">ロードマップを実践して理解のスタート地点に立とう</span>
+              <span class="">ロードマップを実践して"完全に理解した"を目指す</span><br/>
+              <span class="text-xs">※"完全に理解した"とは、「入門しただけでほぼ何も分かっていない」という意味のエンジニア用語</span>
             </h2>
           </div>
           <div class="w-1/2 lg:h-screen flex flex-col justify-normal">
