5259f915e1569713263e3a94bc7fa88ed8a596ff
Midori Takahashi <mintgreen7programming@gmail.com>
2024-03-20 15:21:01 +0900

    Remove function

--- functions/_middleware.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  try {
-    return await context.next();
-  } catch (err) {
-    return new Response(`${err.message}\n${err.stack}`, { status: 500 });
-  }
-}
