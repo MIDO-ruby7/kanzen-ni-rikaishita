c473e9813e172d7352f08ccad29159512b5b35c0
Midori Takahashi <mintgreen7programming@gmail.com>
2024-03-22 06:16:46 +0900

    getPlatformProxy setting

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import build from '@hono/vite-cloudflare-pages'
 import { defineConfig } from 'vite'
-
+import { getPlatformProxy } from 'wrangler'
 
 export default defineConfig( async () => {
+  const { env } = await getPlatformProxy()
   return {
     plugins: [
       devServer({
         entry: 'src/index.tsx',
         adapter,
+        env,
       }),
       build({
         entry: 'src/index.tsx',
