4eb82a0e4e5413110d46886324f53e477cfd591e
Midori Takahashi <mintgreen7programming@gmail.com>
2024-03-25 21:56:19 +0900

    getPlatformProxyが使用できないか再度検証

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,31 @@ import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import pagesBuild from '@hono/vite-cloudflare-pages'
 import { defineConfig } from 'vite'
+import { getPlatformProxy } from 'wrangler'
 
 export default defineConfig(async ({ command }) => {
   if (command === 'build') {
     return {
       plugins: [pagesBuild()]
     }
-  }
-  return {
-    plugins: [
-      devServer({
-        entry: 'src/index.tsx',
-        env: process.env,
-        adapter,
-      }),
-      pagesBuild({
-        entry: 'src/index.tsx',
-      }),
-    ]
+  } else {
+    const { env, dispose } = await getPlatformProxy();
+    return {
+      plugins: [
+        devServer({
+          entry: 'src/index.tsx',
+          env,
+          adapter,
+          plugins: [
+            {
+              onServerClose: dispose
+            }
+          ]
+        }),
+        pagesBuild({
+          entry: 'src/index.tsx',
+        }),
+      ]
+    }
   }
 })
